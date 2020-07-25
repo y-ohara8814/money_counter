3c58818d27050bb2c71c3a6b5a62a15dc2605121
ohara <yohara8814@gmail.com>
2020-07-25 15:55:56 +0900

    グループのバリデーション設定（途中）

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,5 +4,7 @@ class Group < ApplicationRecord
     has_many :plans, foreign_key:"group_id", dependent: :destroy
     accepts_nested_attributes_for :group_users, allow_destroy: true
 
-    validates :name, presence: {message: "グループ名は必須です"}, length: {maximum: 15}
+    validates :name, presence: {message: "グループ名は必須です"}, length: {maximum: 30}
+
+    # validates :user_ids, presence: {message: "所属ユーザーは自分を含め１名以上選択してください"}
 end
--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -2,5 +2,4 @@ class GroupUser < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :group, optional: true
 
-  validates :user_id, presence: true
 end
